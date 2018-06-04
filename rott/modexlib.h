59703bb43d2cf3cb684656330e376fc550b18b49
Marc-Alexandre Espiaut <marcalexandre@member.fsf.org>
2018-06-04 15:27:49 +0200

    Removing DOS code modexlib.h

--- rott/modexlib.h
+++ rott/modexlib.h
@@ -46,69 +46,6 @@ int iGLOBAL_SCREENHEIGHT;//bna val 600
 //
 //***************************************************************************
 
-#ifdef DOS
-#define SC_INDEX                0x3C4
-#define SC_DATA                 0x3C5
-#define SC_RESET                0
-#define SC_CLOCK                1
-#define SC_MAPMASK              2
-#define SC_CHARMAP              3
-#define SC_MEMMODE              4
-
-#define CRTC_INDEX              0x3D4
-#define CRTC_DATA               0x3D5
-#define CRTC_H_TOTAL            0
-#define CRTC_H_DISPEND          1
-#define CRTC_H_BLANK            2
-#define CRTC_H_ENDBLANK         3
-#define CRTC_H_RETRACE          4
-#define CRTC_H_ENDRETRACE       5
-#define CRTC_V_TOTAL            6
-#define CRTC_OVERFLOW           7
-#define CRTC_ROWSCAN            8
-#define CRTC_MAXSCANLINE        9
-#define CRTC_CURSORSTART        10
-#define CRTC_CURSOREND          11
-#define CRTC_STARTHIGH          12
-#define CRTC_STARTLOW           13
-#define CRTC_CURSORHIGH         14
-#define CRTC_CURSORLOW          15
-#define CRTC_V_RETRACE          16
-#define CRTC_V_ENDRETRACE       17
-#define CRTC_V_DISPEND          18
-#define CRTC_OFFSET             19
-#define CRTC_UNDERLINE          20
-#define CRTC_V_BLANK            21
-#define CRTC_V_ENDBLANK         22
-#define CRTC_MODE               23
-#define CRTC_LINECOMPARE        24
-
-#define GC_INDEX                0x3CE
-#define GC_DATA                 0x3CF
-#define GC_SETRESET             0
-#define GC_ENABLESETRESET       1
-#define GC_COLORCOMPARE         2
-#define GC_DATAROTATE           3
-#define GC_READMAP              4
-#define GC_MODE                 5
-#define GC_MISCELLANEOUS        6
-#define GC_COLORDONTCARE        7
-#define GC_BITMASK              8
-
-#define ATR_INDEX               0x3c0
-#define ATR_MODE                16
-#define ATR_OVERSCAN            17
-#define ATR_COLORPLANEENABLE      18
-#define ATR_PELPAN              19
-#define ATR_COLORSELECT         20
-
-#define STATUS_REGISTER_1       0x3da
-
-#define PEL_WRITE_ADR           0x3c8
-#define PEL_READ_ADR            0x3c7
-#define PEL_DATA                0x3c9
-#endif
-
 extern  boolean StretchScreen;//bn�++
 
 //extern  int      ylookup[MAXSCREENHEIGHT];      // Table of row offsets
@@ -167,14 +104,8 @@ void  TurnOffTextCursor ( void );
         modify exact [eax edx]
 #endif
 
-#ifdef DOS
-void  VGAMAPMASK (int x);
-void  VGAREADMAP (int x);
-void  VGAWRITEMAP(int x);
-#else
 #define VGAMAPMASK(a)
 #define VGAREADMAP(a)
 #define VGAWRITEMAP(a)
-#endif
 
 #endif
