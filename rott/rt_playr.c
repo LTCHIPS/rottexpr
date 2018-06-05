af8d4e041098a4453c2670cabfc09bd2c3a7df39
Marc-Alexandre Espiaut <marcalexandre@member.fsf.org>
2018-06-05 12:03:15 +0200

    Removing unused but set variables in rt_playr.c

--- rott/rt_playr.c
+++ rott/rt_playr.c
@@ -555,10 +555,6 @@ void RevivePlayerobj (int tilex, int tiley, int dir, objtype*ob)
 */
 void SpawnPlayerobj (int tilex, int tiley, int dir, int playerindex)
 {
-    playertype *pstate;
-
-    pstate = &PLAYERSTATE[playerindex];
-
     GetNewActor();
     MakeActive(new);
 
@@ -5717,7 +5713,6 @@ void CheckTemp2Codes(objtype *ob,playertype *pstate)
 {
     int pitheight;
     int godheight;
-    int dogheight;
     int height;
     int oldz;
     int destheightoffset;
@@ -5725,7 +5720,6 @@ void CheckTemp2Codes(objtype *ob,playertype *pstate)
 
     pitheight    = maxheight - 8;
     height = ob->z + pstate->playerheight;
-    dogheight    = ob->z + DOGOFFSET;
     godheight    = ob->z + GODOFFSET;
 
     //SoftError("\nheightoffset: %d, temp2: %d",pstate->heightoffset,ob->temp2);
